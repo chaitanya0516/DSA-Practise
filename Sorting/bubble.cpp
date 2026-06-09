c8a9a2ff92c9b642839cef44de5172c69a453673
chaitanya0516 <chaitanya05singh@gmail.com>
2026-06-09 19:09:00 +0530

    Add sorting and hashing files

--- Sorting/bubble.cpp
+++ Sorting/bubble.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void bubble_sort(int arr[] , int n){
     for(int i = n-1;i >=0;i--){
-        for(int j=0;j<=i;j++){
+        for(int j=0;j<i;j++){
             if(arr[j]>arr[j+1]){
             int temp = arr[j+1];
             arr[j+1] = arr[j];
@@ -18,7 +18,7 @@ int main(){
     int n;
     cin>>n;
     int arr[n];
-    for(int i = 0; i,n; i++) cin >> arr[i];
+    for(int i = 0; i<n; i++) cin >> arr[i];
     bubble_sort(arr,n);
     for(int i = 0; i<n;i++){
         cout<<arr[i] << " ";
