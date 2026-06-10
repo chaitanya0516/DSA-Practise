813808df26d51ea41a18b8759a5b42ebe1d96827
chaitanya0516 <chaitanya05singh@gmail.com>
2026-06-10 21:57:50 +0530

    Add array files

--- Array/SecondLargestElement.cpp
+++ Array/SecondLargestElement.cpp
@@ -1,6 +1,49 @@
+// Better Solution
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cin>>n;  
+//     int arr[n];
+//     for(int i = 0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int largest = arr[0];
+//     for(int i = 0;i<n;i++){
+//         if(arr[i]>largest)
+//         largest = arr[i];
+//     }
+//     int slargest = -1;
+//     for(int i = 0;i<n;i++){
+//         if(arr[i]>slargest && arr[i]!=largest)
+//         slargest = arr[i];
+//     }
+//     cout<<slargest<<endl;
+//     return 0;
+// }
+
+// Optimal Solution
+// We are founding both second largest and 2nd shortest element in the array
 #include<bits/stdc++.h>
 using namespace std;
-
 int main(){
-    
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0;i<n;i++)
+    cin>> arr[i];
+
+    int largest = arr[0];
+    int slargest = INT_MIN;
+    for(int i = 1;i<n;i++){
+        if (arr[i] > largest){
+            slargest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] < largest && arr[i] > slargest){
+            slargest = arr[i];
+        }
+    }
+    return slargest;
 }
--- Sorting/insertion.cpp
+++ Sorting/insertion.cpp
@@ -20,7 +20,7 @@ int main(){
     int n;
     cin>>n;
     int arr[1000];
-    for(int i = 0; i,n; i++) cin >> arr[i];
+    for(int i = 0; i<n; i++) cin >> arr[i];
     insertion_sort(arr,n);
     for(int i = 0; i<n;i++){
         cout<<arr[i] << " ";
