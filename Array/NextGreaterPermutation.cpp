37d55bd1c8e73687695f63cc541dde589311949b
chaitanya0516 <chaitanya05singh@gmail.com>
2026-06-14 20:06:30 +0530

    Rearrange

---
+++ Array/NextGreaterPermutation.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> nextGreaterPermutation(vector<int> &A) {
+    int ind = -1;
+    int n =A.size();
+    for(int i = n-2;i>=0;i--){
+        if(A[i])
+    }
+}
