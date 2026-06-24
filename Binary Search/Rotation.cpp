8bb65d9505320c04b1cc83e2762a17d07623b641
chaitanya0516 <chaitanya05singh@gmail.com>
2026-06-24 13:19:30 +0530

    Rotation

---
+++ Binary Search/Rotation.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int findMin(vector<int>&arr){
+    int low = 0,high = arr.size()-1;
+    int ans = INT_MAX;
+    int index = -1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        // search space is already sorted
+        // then always arr[low] is will be smaller
+        // in that search space
+        if(arr[low]<=arr[high]){
+            if(arr[low]<ans){
+                index = low;
+                ans = arr[low];
+            }
+            break;
+        }
+        if(arr[low]<= arr[mid]){
+            if(arr[low]<ans){
+                index = low;
+                ans = arr[low];
+            }
+        low = mid +1;
+        }
+        else{
+            high = mid -1;
+            if(arr[mid]<ans){
+                index = mid;
+                ans = arr[mid];
+        }
+        }
+    }
+    return index;
+}
