2ff028b6e3a9a9d48cdb7ddb0a28153df35336ef
chaitanya0516 <chaitanya05singh@gmail.com>
2026-06-27 20:54:04 +0530

    kth missing +ve number

---
+++ Binary Search/kthMissingPositiveNumber.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int missingK(vector<int>& arr,int k){
+    int low = 0, high = arr.size()-1;
+    while(low<=high){
+        int mid = low+(high-low)/2;
+        int missing = arr[mid] - (mid+1);
+        if(missing<k){ 
+             low = mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+    return (high + 1 +k);
+
+}
